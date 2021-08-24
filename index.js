192b11d4572f31368ab6e8af9afece813fe806e8
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-08-24 13:41:12 -0400

    Instructions added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=52')
+.then(resp => resp.json())
+.then(data =>console.log(data))
+
 const getName = document.querySelector('#bttn')
 const getNameValue = document.querySelector('#name')
 const putNameHere = document.querySelector('#nameInput')
 getName.addEventListener('submit', event => {
     event.preventDefault()
-    
+
 
 })

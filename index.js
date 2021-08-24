e60273f4480bbca8bb7a1da270b7f929fd2c96c6
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-08-24 09:33:22 -0400

    Tuesday modifications

---
+++ index.js
@@ -0,0 +1,8 @@
+const getName = document.querySelector('#bttn')
+const getNameValue = document.querySelector('#name')
+const putNameHere = document.querySelector('#nameInput')
+getName.addEventListener('submit', event => {
+    event.preventDefault()
+    
+
+})

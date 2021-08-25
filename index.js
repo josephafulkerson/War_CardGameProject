b5762c369445d1115bb96380f8e92e8348d7cd20
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-08-24 20:11:30 -0400

    test

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=52')
 .then(resp => resp.json())
 .then(data =>console.log(data))
 
-const getName = document.querySelector('#bttn')
-const getNameValue = document.querySelector('#name')
-const putNameHere = document.querySelector('#nameInput')
-getName.addEventListener('submit', event => {
-    event.preventDefault()
+// First thing -> player types in name
+e.preventDefault()
 
-
+grabValue.addEventListener('submit', e => {
+    const grabPlayer = document.querySelector('#nameInput')
+    const grabValue = document.querySelector('#name')
+    //const grabBttn = document.querySelector('#bttn')
+    grabPlayer.innerHTML = grabValue
 })
+
+
+
+// const getName = document.querySelector('#bttn')
+// const getNameValue = document.querySelector('#name')
+// const putNameHere = document.querySelector('#nameInput')
+// getName.addEventListener('submit', event => {
+//     event.preventDefault()
+
+
+// })

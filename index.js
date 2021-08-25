55e8657fdebc893e0a684fe2004c37d0854d9032
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-08-24 23:02:44 -0400

    render cards and populate images

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=52')
 .then(resp => resp.json())
-.then(data => {
-    
-})
+.then(data =>  {
+    // card images
+    //let currentTopCard // url
+    //let currentBotCard // url
+    //let cards
+    //cardImg[0]
+    //cardImg[1]
+    const cardImg = Object.values(data.cards)
+    renderCards(cardImg)
+
+function renderCards(cardImg) {
+    const botCard = document.querySelector('#sqr2')
+    const playerCard = document.querySelector('#sqr3')
+
+    const dealCard = document.querySelector('#sqr')
+    dealCard.addEventListener('click', event => {
+        cardImg.forEach(element => {
+            const selectBotCard = document.querySelector('#botCard')
+            const selectPlayerCard = document.querySelector('#playerCard')
+            selectBotCard.src = element.image
+            selectPlayerCard.src = element.image
+        })
+    })
+    //click event to 'deal'
+    //iterate through to display images
+}
 
 // First thing -> player types in name
 const grab = document.querySelector('#form')
@@ -13,17 +36,4 @@ grab.addEventListener('submit', e => {
     const grabBttn = document.querySelector('#bttn')
     grabPlayer.textContent=grabValue.value
 })
-
-//populate cards
-
-
-
-// const getName = document.querySelector('#bttn')
-// const getNameValue = document.querySelector('#name')
-// const putNameHere = document.querySelector('#nameInput')
-// getName.addEventListener('submit', event => {
-//     event.preventDefault()
-// })
-
-// have cards populate
-//create click event that 'deals' cards
+})
